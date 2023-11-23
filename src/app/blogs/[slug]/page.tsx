203cc3ec0c642c8a0371cc0f439fd5fb7fe1d06b
ned2070te <ned2070te@gmail.com>
2023-11-23 23:33:43 +0000

    nitial commit

---
+++ src/app/blogs/[slug]/page.tsx
@@ -0,0 +1,31 @@
+import { getPostsBySlug } from "@/lib/posts";
+import { getPosts } from "@/lib/posts";
+import { notFound } from "next/navigation";
+
+type blogPostParams = {
+  params: {
+    slug: string;
+  };
+};
+
+export function generateStaticParams() {
+  const posts = getPosts();
+  return posts.map((post) => {
+    return { slug: post.slug };
+  });
+}
+
+export default function Page({ params }: blogPostParams) {
+  const post = getPostsBySlug(params.slug);
+
+  if (!post) {
+    notFound();
+  }
+
+  return (
+    <div>
+      <h1>{post.title}</h1>
+      <p>{post.content}</p>
+    </div>
+  );
+}

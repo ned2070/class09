90c5e2f07b4dd47bcb53767c7a4a53b313fff9fa
ned2070te <ned2070te@gmail.com>
2023-11-24 09:41:31 +0000

    home page linked to blogs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>This is the home page. </h1>
+      <h1>
+        <Link href="blogs">Go to blogs</Link>
+      </h1>
     </main>
   );
 }
